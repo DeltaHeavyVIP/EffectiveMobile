b476108585e1514717665726b0f09afd221cbdbf
DeltaHeavyVIP <patutin.vovan@mail.ru>
2023-08-15 17:27:00 +0300

    create project

---
+++ src/main/java/com/example/introductory/IntroductoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.introductory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroductoryApplication.class, args);
+    }
+
+}
